3b51cf90a7cbe0a3bcd828a73a1250375535f6e1
Atif Iqbal <atif.ahangar@stonybrook.edu>
2017-10-18 19:59:45 -0400

    Part3 dut passing simple tests.

---
+++ part3/defines_pkg.sv
@@ -0,0 +1,19 @@
+// ------------------------------------------//
+// Matrix Vector Multiplier Defines Part-1
+// ------------------------------------------//
+// NAME:  Atif Iqbal
+// NETID: aahangar
+// SBUID: 111416569
+// ------------------------------------------//
+
+package defines_pkg;
+
+    parameter NROWS_A = 4;
+    parameter NCOLS_A = 4;
+    parameter NROWS_B = 4;
+    parameter NCOLS_B = 1;
+
+    parameter NUM_S   = 2;
+    parameter VEC_S   = 4;
+
+endpackage
