5a56a1408cdcde6c59b3c82f1aaff93c2477f391
Atif Iqbal <atif.ahangar@stonybrook.edu>
2017-10-18 19:37:17 -0400

    Part 2 simple cases working.

---
+++ part2/mvm3_part2.sv
@@ -0,0 +1,270 @@
+// ------------------------------------------//
+// Matrix Vector Multiplier Part-1
+// ------------------------------------------//
+// NAME:  Atif Iqbal
+// NETID: aahangar
+// SBUID: 111416569
+// ------------------------------------------//
+
+import defines_pkg::*;
+
+module mvm3_part1 #(parameter NROWS_A = NROWS_A,
+                    parameter NCOLS_A = NCOLS_A,
+                    parameter NROWS_B = NROWS_B,
+                    parameter NCOLS_B = NCOLS_B)
+(
+    input  logic                clk,
+    input  logic                reset,
+    input  logic                s_valid,
+    input  logic                m_ready,
+    input  logic signed [7:0]   data_in,
+    output logic signed [15:0]  data_out,
+    output logic                m_valid,
+    output logic                s_ready,
+    output logic                overflow
+);
+
+    localparam MAT_A_SIZE  = NROWS_A * NCOLS_A;
+    localparam MAT_B_SIZE  = NROWS_B * NCOLS_B;
+    localparam MAT_A_LSIZE = $clog2(MAT_A_SIZE);
+    localparam MAT_B_LSIZE = $clog2(MAT_B_SIZE);
+
+    enum logic [1:0] {WRITE_A=0, WRITE_B=1, WRITE_X=2, READ=3} state, next_state;
+
+    logic        [MAT_A_LSIZE-1:0] addr_a;
+    logic        [MAT_A_LSIZE-1:0] wr_addr_a;
+    logic        [MAT_A_LSIZE-1:0] rd_addr_a;
+    logic        [MAT_B_LSIZE-1:0] addr_b;
+    logic        [MAT_B_LSIZE-1:0] wr_addr_b;
+    logic        [MAT_B_LSIZE-1:0] rd_addr_b;
+    logic        [MAT_B_LSIZE-1:0] addr_x;
+    logic        [MAT_B_LSIZE-1:0] wr_addr_x;
+    logic        [MAT_B_LSIZE-1:0] rd_addr_x;
+    logic signed             [7:0] data_out_a;
+    logic signed             [7:0] data_out_b;
+    logic                    [7:0] data_out_x;
+    logic                          wr_en_a;
+    logic                          wr_en_b;
+    logic                          wr_en_x;
+    logic                    [1:0] vec_cnt;
+
+    logic signed             [7:0] mac1_a;
+    logic signed             [7:0] mac1_b;
+    logic                    [7:0] mac1_x;
+    logic                          mac1_valid_in;
+    logic signed            [15:0] mac1_f;
+    logic                          mac1_valid_out;
+    logic                          mac1_overflow;
+
+    logic                          next_req;
+    logic                          valid_int;
+    logic                          overflow_int;
+
+    assign data_out      = mac1_f;
+    assign m_valid       = valid_int;
+    assign overflow      = overflow_int;
+    assign mac1_a        = data_out_a;
+    assign mac1_b        = data_out_b;
+    assign mac1_x        = data_out_x;
+
+    assign addr_a   = (state == WRITE_A) ? wr_addr_a : rd_addr_a;
+    assign addr_b   = (state == WRITE_B) ? wr_addr_b : rd_addr_b;
+    assign addr_x   = (state == WRITE_X) ? wr_addr_x : rd_addr_x;
+    assign wr_en_a  = (state == WRITE_A) & s_valid;
+    assign wr_en_b  = (state == WRITE_B) & s_valid;
+    assign wr_en_x  = (state == WRITE_X) & s_valid;
+
+    memory #(
+        .WIDTH    ( 8               ),
+        .SIZE     ( MAT_A_SIZE      ),
+        .LOGSIZE  ( MAT_A_LSIZE     ))
+    u_mat_a_mem (
+        .clk      ( clk             ),
+        .data_in  ( data_in         ),
+        .data_out ( data_out_a      ),
+        .addr     ( addr_a          ),
+        .wr_en    ( wr_en_a         ));
+
+    memory #(
+        .WIDTH    ( 8               ),
+        .SIZE     ( MAT_B_SIZE      ),
+        .LOGSIZE  ( MAT_B_LSIZE     ))
+    u_mat_b_mem (
+        .clk      ( clk             ),
+        .data_in  ( data_in         ),
+        .data_out ( data_out_b      ),
+        .addr     ( addr_b          ),
+        .wr_en    ( wr_en_b         ));
+
+    memory #(
+        .WIDTH    ( 8               ),
+        .SIZE     ( MAT_B_SIZE      ),
+        .LOGSIZE  ( MAT_B_LSIZE     ))
+    u_mat_x_mem (
+        .clk      ( clk             ),
+        .data_in  ( data_in         ),
+        .data_out ( data_out_x      ),
+        .addr     ( addr_x          ),
+        .wr_en    ( wr_en_x         ));
+
+    part3_mac #(
+        .NUM_S     ( 2              ))
+    u_mac_1 (
+        .clk       ( clk            ),
+        .reset     ( reset          ),
+        .a         ( mac1_a         ),
+        .b         ( mac1_b         ),
+        .x         ( mac1_x         ),
+        .valid_in  ( mac1_valid_in  ),
+        .f         ( mac1_f         ),
+        .valid_out ( mac1_valid_out ),
+        .overflow  ( mac1_overflow  ));
+
+    //FSM
+    always_ff @(posedge clk)
+        if(reset) begin
+            state <= WRITE_A;
+        end
+        else begin
+            state <= next_state;
+        end
+
+    always_comb begin
+        next_state = WRITE_A;
+
+        case (state)
+
+            WRITE_A: begin
+                if(wr_addr_a == MAT_A_SIZE-1 && s_valid)
+                    next_state = WRITE_B;
+                else
+                    next_state = WRITE_A;
+            end
+
+            WRITE_B: begin
+                if(wr_addr_b == MAT_B_SIZE-1 && s_valid)
+                    next_state = WRITE_X;
+                else
+                    next_state = WRITE_B;
+            end
+
+            WRITE_X: begin
+                if(wr_addr_x == MAT_B_SIZE-1 && s_valid)
+                    next_state = READ;
+                else
+                    next_state = WRITE_X;
+            end
+
+            READ: begin
+                if(rd_addr_a == MAT_A_SIZE-1 && rd_addr_b == MAT_B_SIZE-1 && next_req)
+                    next_state = WRITE_A;
+                else
+                    next_state = READ;
+            end
+
+        endcase
+    end
+
+    always_ff @(posedge clk)
+        if(reset) begin
+            wr_addr_a <= 'd0;
+            wr_addr_b <= 'd0;
+            wr_addr_x <= 'd0;
+        end
+        else begin
+            if (wr_addr_a == MAT_A_SIZE-1 && s_valid) begin
+                wr_addr_a <= 'd0;
+            end
+            else if (wr_addr_b == MAT_B_SIZE-1 && s_valid) begin
+                wr_addr_b <=  'd0;
+            end
+            else if (wr_addr_x == MAT_B_SIZE-1 && s_valid) begin
+                wr_addr_x <=  'd0;
+            end
+            else if (state == WRITE_A && s_valid) begin
+                wr_addr_a <= wr_addr_a + 1'd1;
+            end
+            else if (state == WRITE_B && s_valid) begin
+                wr_addr_b <= wr_addr_b + 1'd1;
+            end
+            else if (state == WRITE_X && s_valid) begin
+                wr_addr_x <= wr_addr_x + 1'd1;
+            end
+        end
+
+    always_ff @(posedge clk)
+        if(reset) begin
+            rd_addr_a <= 'd0;
+            rd_addr_b <= 'd0;
+            rd_addr_x <= 'd0; //TODO: can use rd_addr_b instead. Keeping now for consistency.
+        end
+        else begin
+            if (rd_addr_a == MAT_A_SIZE-1 && next_req) begin
+                rd_addr_a <= 'd0;
+                rd_addr_b <= 'd0;
+                rd_addr_x <= 'd0;
+            end
+            else if (rd_addr_b == MAT_B_SIZE-1 && next_req) begin
+                rd_addr_a <= rd_addr_a + 1'd1;
+                rd_addr_x <= rd_addr_x + 1'd1;
+                rd_addr_b <= 'd0;
+            end
+            else if ((state == READ) && vec_cnt < 2'd3 && next_req) begin
+                rd_addr_a <= rd_addr_a + 1'd1;
+                rd_addr_b <= rd_addr_b + 1'd1;
+            end
+        end
+
+    always_ff @(posedge clk)
+        if(reset) begin
+            next_req      <= 1'b1;
+            mac1_valid_in <= 1'b0;
+            vec_cnt       <=  'd0;
+        end
+        else begin
+            if (vec_cnt == 2'd3) begin
+                next_req      <= 1'b0;
+                mac1_valid_in <= 1'b0;
+                vec_cnt       <= 2'd0;
+            end
+            else if (m_valid && m_ready) begin
+                next_req      <= 1'b1;
+            end
+            else if (next_req && (state == READ)) begin
+                next_req      <= 1'b1;
+                mac1_valid_in <= 1'b1;
+                vec_cnt       <= vec_cnt + 1'd1;
+            end
+        end
+
+    always_ff @(posedge clk)
+        if(reset) begin
+            valid_int <= 1'b0;
+            overflow_int <= 1'b0;
+        end
+        else begin
+            if (mac1_valid_out) begin
+                valid_int <= 1'b1;
+                overflow_int <= mac1_overflow;
+            end
+            else if (valid_int && m_ready) begin
+                valid_int <= 1'b0;
+                overflow_int <= 1'b0;
+            end
+        end
+
+    always_ff @(posedge clk)
+        if(reset) begin
+            s_ready     <= 'd1;
+        end
+        else begin
+            if (state == READ) begin
+                s_ready <= 'd0;
+            end
+            else begin
+                s_ready <= 'd1;
+            end
+        end
+
+endmodule
+//end of file.
---
+++ part2/mvm3_part2_tb.sv
@@ -0,0 +1,188 @@
+//-------------------------------------------//
+// Matrix Vector Multiplier Testbench Part-1
+// ------------------------------------------//
+// NAME:  Atif Iqbal
+// NETID: aahangar
+// SBUID: 111416569
+// ------------------------------------------//
+
+import defines_pkg::*;
+
+class mat_mult_model;
+
+    localparam M_SIZE_A = NROWS_A * NCOLS_A;
+    localparam M_SIZE_B = NROWS_B * NCOLS_B;
+    localparam M_SIZE_Z = NROWS_A * NCOLS_B;
+
+    rand bit signed   [ 7:0] mat_a_in[M_SIZE_A];
+    rand bit signed   [ 7:0] mat_b_in[M_SIZE_B];
+    rand bit          [ 7:0] mat_x_in[M_SIZE_B];
+    
+    bit signed        [15:0] int_a;
+    bit signed        [15:0] int_b;
+
+    bit signed        [ 7:0] mat_a[M_SIZE_A][*];
+    bit signed        [ 7:0] mat_b[M_SIZE_B][*];
+    bit               [ 7:0] mat_x[M_SIZE_B][*];
+    bit signed        [15:0] mat[M_SIZE_Z][*];
+    bit                      ovf[NCOLS_A][*];
+
+    static int idx = 0;
+    
+    function void post_randomize();
+    begin
+        for(int i = 0; i < M_SIZE_A; i++) begin
+            mat_a[i][idx] = mat_a_in[i];
+        end
+        for(int i = 0; i < M_SIZE_B; i++) begin
+            mat_b[i][idx] = mat_b_in[i];
+        end
+        for(int i = 0; i < M_SIZE_B; i++) begin
+            mat_x[i][idx] = mat_x_in[i];
+        end
+        for(int i = 0; i < NCOLS_A; i++) begin
+            mat[i][idx] = mat_x[i][idx];
+            for(int j = 0; j < NCOLS_A; j++) begin
+                int_a = mat_a[j+i*NCOLS_A][idx] * mat_b[j][idx];
+                int_b = mat[i][idx] + int_a;
+                ovf[i][idx] = ((mat[i][idx] > 0 && int_a > 0 && int_b < 0) ||
+                               (mat[i][idx] < 0 && int_a < 0 && int_b > 0));
+                mat[i][idx] = int_b;
+            end
+        end
+        idx++;
+    end
+    endfunction
+endclass
+
+module tb_part2_mvm();
+
+    localparam M_SIZE_A = NROWS_A * NCOLS_A;
+    localparam M_SIZE_B = NROWS_B * NCOLS_B;
+    localparam M_SIZE_Z = NROWS_A * NCOLS_B;
+
+    logic               clk; 
+    logic               reset;
+    logic               s_valid;
+    logic               m_ready;
+    logic signed [7:0]  data_in;
+    logic signed [15:0] data_out;
+    logic               m_valid;
+    logic               s_ready;
+    logic               overflow;
+
+    int idr;
+    int idx;
+    int j;
+    int num_trans = 4000;
+    
+    mvm3_part1 #(
+        .NROWS_A ( NROWS_A ),
+        .NCOLS_A ( NCOLS_A ),
+        .NROWS_B ( NROWS_B ),
+        .NCOLS_B ( NCOLS_B )) 
+    dut(.*);
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    mat_mult_model matm_m = new();
+
+    initial begin
+
+        // Before first clock edge, initialize
+        reset   = 1;
+        s_valid = 0;
+        m_ready = 0;
+        data_in = 0;
+
+        if (reset) begin
+           @(posedge clk);
+           #1;
+           $display("Reseting DUT!");
+           reset = 0; 
+        end
+
+        for (int i = 0; i < num_trans; i++) begin
+            if(overflow) $display("Overflow Dectected By DUT!");
+            matm_m.randomize();
+            data_in = matm_m.mat_a_in[0];
+            for (j = 0; j < M_SIZE_A; ) begin
+                @(posedge clk);
+                #1;
+                j = (s_valid && s_ready) ? j+1 : j;
+                data_in = matm_m.mat_a_in[j];
+                std::randomize(s_valid, m_ready);
+            end
+            j = 0;
+            data_in = matm_m.mat_b_in[j];
+            for (j = 0; j < M_SIZE_B; ) begin
+                @(posedge clk);
+                #1;
+                j = (s_valid && s_ready) ? j+1 : j;
+                data_in = matm_m.mat_b_in[j];
+                std::randomize(s_valid, m_ready);
+            end
+            j = 0;
+            data_in = matm_m.mat_x_in[j];
+            for (j = 0; j < M_SIZE_B; ) begin
+                @(posedge clk);
+                #1;
+                j = (s_valid && s_ready) ? j+1 : j;
+                data_in = matm_m.mat_x_in[j];
+                std::randomize(s_valid, m_ready);
+            end
+        end
+
+        s_valid = 0;
+        m_ready = 1;
+
+        wait(idx == num_trans);
+
+        #10 $display("!!!Verification PASSED!!!");
+
+        #10 $finish();
+
+    end // initial begin
+
+    always @(posedge clk) begin
+        if(m_valid && m_ready && !reset) begin
+
+            if(idr == 0) begin
+                $display("------Transaction Data Start--------");
+                $display("Matrix A:");
+                for(int i = 0; i < M_SIZE_A; i++) begin
+                    $write("%d ", matm_m.mat_a[i][idx]);
+                    if((i+1) % NCOLS_A == 0) $write("\n");
+                end
+                $display("Matrix B:");
+                for(int i = 0; i < M_SIZE_B; i++) begin
+                    $write("%d ", matm_m.mat_b[i][idx]);
+                end
+                $write("\n");
+                $display("Matrix X:");
+                for(int i = 0; i < M_SIZE_B; i++) begin
+                    $write("%d ", matm_m.mat_x[i][idx]);
+                end
+                $write("\n");
+                $display("Matrix Z:");
+                for(int i = 0; i < NCOLS_A; i++) begin
+                    $write("%d ", matm_m.mat[i][idx]);
+                end
+                $write("\n");
+                $display("------Transaction Data End----------");
+            end
+            
+            if(data_out == matm_m.mat[idr][idx])
+                $display("PASSED - IDX: %d, IDR: %d, Output: %d, Exp Output: %d", idx, idr, data_out, matm_m.mat[idr][idx]);
+            else begin
+                $display("FAILED - IDX: %d, IDR: %d, Output: %d, Exp Output: %d", idx, idr, data_out, matm_m.mat[idr][idx]);
+                $display("!!!Verification FAILED!!!");
+                $finish();
+            end
+            idx = (idr == NCOLS_A-1) ? idx+1 : idx;
+            idr = (idr == NCOLS_A-1) ? 0 : idr+1;
+        end
+    end
+
+endmodule // tb_part2_mac
